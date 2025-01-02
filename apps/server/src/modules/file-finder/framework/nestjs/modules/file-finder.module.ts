ab2f16c443b4bec15fabe0ee3e175c4a8617bb21
Pavel Balashov <pavel.balashov1@gmail.com>
2025-01-02 13:18:13 +0100

    chore(server): export FileFinderNestJsService via FileFinderService token in FileFinderModule

--- apps/server/src/modules/file-finder/framework/nestjs/modules/file-finder.module.ts
+++ apps/server/src/modules/file-finder/framework/nestjs/modules/file-finder.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { FileFinderNestJsService } from '../services/file-finder.nestjs-service';
+import { FileFinderService } from '../../../infrastructure/services/file-finder.service';
 
 @Module({
   imports: [],
   providers: [FileFinderNestJsService],
-  exports: [FileFinderNestJsService],
+  exports: [
+    {
+      provide: FileFinderService,
+      useExisting: FileFinderNestJsService,
+    },
+  ],
 })
 export class FileFinderModule {}
