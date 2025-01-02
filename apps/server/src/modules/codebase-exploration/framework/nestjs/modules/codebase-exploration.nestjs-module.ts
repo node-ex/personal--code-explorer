0e3ee8449f3eb61cb337c4ca4a29fc9fe5403537
Pavel Balashov <pavel.balashov1@gmail.com>
2025-01-02 13:34:24 +0100

    refactor(server): rename NestJS specific files in the codebase exploration module

--- apps/server/src/modules/codebase-exploration/framework/nestjs/modules/codebase-exploration.nestjs-module.ts
+++ apps/server/src/modules/codebase-exploration/framework/nestjs/modules/codebase-exploration.nestjs-module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { codebaseFileEntityTypeormModuleImports } from '../imports/codebase-file-entity-typeorm-module.imports';
+import { codebaseFileEntityTypeormModuleImports } from '../imports/codebase-file-entity-typeorm-module.nestjs-imports';
 import { ICodebaseFileRepositoryOutputPort } from '../../../application/repositories/codebase-file.repository-output-port';
 import { CodebaseFileTypeormNestjsRepositoryOutputAdapter } from '../output-adapters/codebase-file-typeorm.nestjs-repository-output-adapter';
 import { FileFinderModule } from '../../../../file-finder/framework/nestjs/modules/file-finder.module';
--- apps/server/src/modules/core/framework/nestjs/modules/core.module.ts
+++ apps/server/src/modules/core/framework/nestjs/modules/core.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { GreetingModule } from '../../../../greeting/framework/nestjs/modules/greeting.nestjs-module';
 import { rootConfigModuleNestjsImports } from '../../../../../shared/framework/nestjs/imports/root-config-module.nestjs-imports';
 import { rootTypeormModuleNestjsImports } from '../../../../../shared/framework/nestjs/imports/root-typeorm-module.nestjs-imports';
-import { CodebaseExplorationModule } from '../../../../codebase-exploration/framework/nestjs/modules/codebase-exploration.module';
+import { CodebaseExplorationModule } from '../../../../codebase-exploration/framework/nestjs/modules/codebase-exploration.nestjs-module';
 
 @Module({
   imports: [
