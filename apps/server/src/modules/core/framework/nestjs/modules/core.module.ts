f91b6192706664bcd1049bdf9b566bbdb5000577
Pavel Balashov <pavel.balashov1@gmail.com>
2025-01-02 11:25:41 +0100

    refactor(server): rename codebase file processing module to codebase files processing

--- apps/server/src/modules/core/framework/nestjs/modules/core.module.ts
+++ apps/server/src/modules/core/framework/nestjs/modules/core.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { GreetingModule } from '../../../../greeting/framework/nestjs/modules/greeting.nestjs-module';
 import { rootConfigModuleNestjsImports } from '../../../../../shared/framework/nestjs/imports/root-config-module.nestjs-imports';
 import { rootTypeormModuleNestjsImports } from '../../../../../shared/framework/nestjs/imports/root-typeorm-module.nestjs-imports';
-import { CodebaseFileProcessingModule } from '../../../../codebase-file-processing/framework/nestjs/modules/codebase-file-processing.module';
+import { CodebaseFilesProcessingModule } from '../../../../codebase-files-processing/framework/nestjs/modules/codebase-file-processing.module';
 
 @Module({
   imports: [
     ...rootConfigModuleNestjsImports,
     ...rootTypeormModuleNestjsImports,
     GreetingModule,
-    CodebaseFileProcessingModule,
+    CodebaseFilesProcessingModule,
   ],
 })
 export class CoreModule {}
